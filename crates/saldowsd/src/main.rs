8dd02630abb59e0f05c289517d49e6d52af315fe
Kristoffer Andersson <kristoffer.andersson@gu.se>
2025-10-21 12:56:08 +0200

    refactor: introduce batch command as former standard

--- crates/saldowsd/src/main.rs
+++ crates/saldowsd/src/main.rs
@@ -22,58 +22,62 @@ fn main() -> miette::Result<()> {
     //     None => None,
     //     Some(saldo_file) => Some(SaldoLexicon::new(saldo_file)?),
     // };
+    match args.cmd {
+        options::SaldoWsdCmd::AspServer { args } => todo!("start asp-server"),
+        options::SaldoWsdCmd::Batch { args } => {
+            // FIXME: use new_as_shared if more apps are included
+            let wsd = match args.app_name {
+                AppNames::VectorWSD {
+                    decay,
+                    s1_prior,
+                    context_width,
+                    sv_file,
+                    cv_file,
+                } => VectorWSD::new(
+                    &sv_file,
+                    &cv_file,
+                    VectorWSDConfig {
+                        decay,
+                        s1prior: s1_prior,
+                        context_width,
+                    },
+                )?,
+            };
+            // let wsd = make_wsd_application(saldo.as_ref(), &args.app_name, &argv)?;
 
-    // FIXME: use new_as_shared if more apps are included
-    let wsd = match args.app_name {
-        AppNames::VectorWSD {
-            decay,
-            s1_prior,
-            context_width,
-            sv_file,
-            cv_file,
-        } => VectorWSD::new(
-            &sv_file,
-            &cv_file,
-            VectorWSDConfig {
-                decay,
-                s1prior: s1_prior,
-                context_width,
-            },
-        )?,
-    };
-    // let wsd = make_wsd_application(saldo.as_ref(), &args.app_name, &argv)?;
+            // FIXME: eval not supported yet
+            // if args.format == Format::Eval {
+            //     evaluate(wsd, &args.eval_lemmas.unwrap(), &args.eval_key.unwrap());
+            //     return Ok(());
+            // }
 
-    // FIXME: eval not supported yet
-    // if args.format == Format::Eval {
-    //     evaluate(wsd, &args.eval_lemmas.unwrap(), &args.eval_key.unwrap());
-    //     return Ok(());
-    // }
+            // FIXME: ratios not supported yet
+            // let mut ratios = None;
+            // if args.for_lemma.is_some() {
+            //     todo!("ratios is not yet supported");
+            //     // todo!("ratios = Some(HashMap::new())");
+            // }
 
-    // FIXME: ratios not supported yet
-    // let mut ratios = None;
-    // if args.for_lemma.is_some() {
-    //     todo!("ratios is not yet supported");
-    //     // todo!("ratios = Some(HashMap::new())");
-    // }
-
-    let mut stdin = io::stdin().lock();
-    let mut stdout = io::stdout().lock();
-    let format: Box<dyn SourceFormat> = if args.format == Format::Sbxml {
-        todo!("sbxml format is not yet supported");
-    } else {
-        Box::new(TabFormat::default())
-    };
-    disambiguate_sentences(
-        wsd,
-        &mut stdin,
-        &mut stdout,
-        &format,
-        DisambiguateOptions {
-            batch_size: args.batch_size,
-            max_sen: args.max_sen,
-        },
-    )
-    .into_diagnostic()?;
+            let mut stdin = io::stdin().lock();
+            let mut stdout = io::stdout().lock();
+            let format: Box<dyn SourceFormat> = if args.format == Format::Sbxml {
+                todo!("sbxml format is not yet supported");
+            } else {
+                Box::new(TabFormat::default())
+            };
+            disambiguate_sentences(
+                wsd,
+                &mut stdin,
+                &mut stdout,
+                &format,
+                DisambiguateOptions {
+                    batch_size: args.batch_size,
+                    max_sen: args.max_sen,
+                },
+            )
+            .into_diagnostic()?;
+        }
+    }
 
     // FIXME: ratios not supported yet
     // if args.for_lemma.is_some() {
